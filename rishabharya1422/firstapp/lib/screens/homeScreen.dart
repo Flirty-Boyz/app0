fa744f9594c807928523baf22c6dc045b0b05cab
rishabharya1422 <rishabh12536@gmail.com>
2020-02-11 23:58:08 +0530

    0.0.1

---
+++ rishabharya1422/firstapp/lib/screens/homeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+      return MaterialApp(
+          title: "My First APP",
+      );
+  }
+}
