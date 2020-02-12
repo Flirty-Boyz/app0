9f94ae85e3f60c21d6c83d5f42bae037df98f68e
rishabharya1422 <rishabh12536@gmail.com>
2020-02-12 23:34:46 +0530

    udated 0.0.2.1

--- rishabharya1422/firstapp/lib/screens/homeScreen.dart
+++ rishabharya1422/firstapp/lib/screens/homeScreen.dart
@@ -6,8 +6,8 @@ class homeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
 
-      final barColor = const Color(0x9F74B0AF);
-      final bgColor = const Color(0xFFF9DDA4);
+      final barColor = const Color(0x9974B0AF);
+      final bgColor = const Color(0xFFF9D567);
 
       return MaterialApp(
           title: "My First APP",
